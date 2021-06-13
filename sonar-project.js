9f9fc813c4081a0c675fdceadde73e55cb0a1a01
Balaji504 <43697806+Balaji504@users.noreply.github.com>
2021-06-13 11:35:43 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,6 @@ sonarqubeScanner({
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 5a9042965dd734e637fdd0ff656764029ba96508,
             'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
 	  //'sonar.tests': 'specs',
