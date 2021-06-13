6dc2b02534d9bffd72ff06a74d47be6c1ee32ea3
Balaji504 <43697806+Balaji504@users.noreply.github.com>
2021-06-13 18:33:48 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.237.236.207:9000/',
+  serverUrl: 'http://http://52.91.159.184/:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '5a9042965dd734e637fdd0ff656764029ba96508',
+	    'sonar.login': '88f7ad8c777e3fe01fdff984f595939b05e92cc7',
             'sonar.projectVersion':'1.0',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
