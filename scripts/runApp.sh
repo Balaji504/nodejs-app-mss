1044e4b4bd5e5fd9647c80bbc8427f3e37b8c9e2
Balaji504 <43697806+Balaji504@users.noreply.github.com>
2021-06-14 20:02:37 +0530

    Update runApp.sh

--- scripts/runApp.sh
+++ scripts/runApp.sh
@@ -11,4 +11,4 @@ echo $! > .pidfile
 set +x
 
 echo 'Now you can'
-echo 'Visit http://ServerIp:9981 to see your Node.js application'
+echo 'Visit http://3.91.47.169:9981 to see your Node.js application'
